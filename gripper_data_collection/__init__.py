2bb9667f3620e590053012709334aa522c8e1803
Chenbot001 <ejchen2001@gmail.com>
2025-09-12 16:08:09 +0800

    restore

---
+++ gripper_data_collection/__init__.py
@@ -0,0 +1,46 @@
+"""
+Gripper Control System - Modular Architecture Package
+
+This package contains a refactored, modular version of the original 
+gripper_angle_control_encoder.py script.
+
+Modules:
+- main: Main application entry point
+- config: Centralized configuration
+- hardware_manager: Motor communications
+- sensor_manager: Sensor interactions
+- utils: Helper functions
+"""
+
+__version__ = "1.0.0"
+__author__ = "Gripper Control System Team"
+
+from .main_encoder import GripperControlSystem, SystemState
+from .hardware_manager import HardwareManager, StepperMotorManager, GripperMotorManager
+from .sensor_manager import SensorManager, DaimonManager, RotaryEncoderManager
+from .utils import (
+    get_steps_per_degree, angle_to_steps, steps_to_angle,
+    calculate_motor_positions, unwrap_angles,
+    percentage_to_position, position_to_percentage,
+    save_encoder_data_to_csv, create_encoder_plot
+)
+
+__all__ = [
+    'GripperControlSystem',
+    'SystemState', 
+    'HardwareManager',
+    'StepperMotorManager',
+    'GripperMotorManager',
+    'SensorManager',
+    'DaimonManager',
+    'RotaryEncoderManager',
+    'get_steps_per_degree',
+    'angle_to_steps',
+    'steps_to_angle',
+    'calculate_motor_positions',
+    'unwrap_angles',
+    'percentage_to_position',
+    'position_to_percentage',
+    'save_encoder_data_to_csv',
+    'create_encoder_plot'
+]
